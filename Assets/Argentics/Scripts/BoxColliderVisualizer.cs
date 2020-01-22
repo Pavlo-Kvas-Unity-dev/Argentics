b36ecb22181c6452b25f8207fe33c1bceb43f123
Pavlo Kvas <kvaspasha@gmail.com>
2020-01-22 10:46:23 +0200

    create BoxColliderVisualizer script

---
+++ Assets/Argentics/Scripts/BoxColliderVisualizer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class BoxColliderVisualizer : MonoBehaviour
+{
+    public Color highlightColor = Color.green;
+
+
+    public void OnDrawGizmos()
+    {
+        Color prevColor = Gizmos.color;
+
+        Gizmos.color = highlightColor;
+
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+
+        Gizmos.DrawWireCube(transform.position + boxCollider.center, boxCollider.size);
+
+        Gizmos.color = prevColor;
+    }
+
+}
