97f6ea167fad2bfb596daa3f32d89b6572c45c54
Pavlo Kvas <kvaspasha@gmail.com>
2020-01-23 15:50:22 +0200

    shoot only if in range closes #9

--- Assets/Argentics/Scripts/EnemyShooter.cs
+++ Assets/Argentics/Scripts/EnemyShooter.cs
@@ -7,7 +7,8 @@ public class EnemyShooter : MonoBehaviour
 {
     [SerializeField] private GameObject shotPrefab;
     [SerializeField] private Transform BarrelEnd;
-    
+    [SerializeField] private int maxShootDistance = 10;
+
     private float? lastShotTime;
     private float reloadTime = 2f;
     private Transform targetTransform;
@@ -18,13 +19,9 @@ public class EnemyShooter : MonoBehaviour
         Assert.IsNotNull(targetTransform);
     }
 
-    void Start()
-    {
-        FireShot();
-    }
-
     void FixedUpdate()
     {
+        //turn towards the player
         var forward = BarrelEnd.TransformDirection(Vector3.forward);
 
         Vector3 toTarget = targetTransform.position - transform.position;
@@ -33,8 +30,17 @@ public class EnemyShooter : MonoBehaviour
         {
             TurnAround();
         }
-         
-        FireShot();
+
+        //check if player in shooting range
+        var hits = Physics.RaycastAll(new Ray(BarrelEnd.position, BarrelEnd.forward), maxShootDistance);
+        foreach (var raycastHit in hits)
+        {
+            if(raycastHit.transform.CompareTag("Player"))
+            {
+                FireShot();
+            }
+        }
+       
     }
 
     private void FireShot()
