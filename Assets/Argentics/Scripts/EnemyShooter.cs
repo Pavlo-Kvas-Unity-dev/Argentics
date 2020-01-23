b56f1636c8b3ccda96a7e8f303a5fae9350893a8
Pavlo Kvas <kvaspasha@gmail.com>
2020-01-23 13:38:56 +0200

    fire shots

---
+++ Assets/Argentics/Scripts/EnemyShooter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : MonoBehaviour
+{
+    [SerializeField] private GameObject shotPrefab;
+    [SerializeField] private Transform BarrelEnd;
+    
+    private float? lastShotTime;
+    private float reloadTime = 2f;
+
+    void Start()
+    {
+        FireShot();
+    }
+
+    void FixedUpdate()
+    {
+        FireShot();
+    }
+
+    private void FireShot()
+    {
+        if (lastShotTime == null || Time.time - lastShotTime > reloadTime)
+        {
+            lastShotTime = Time.time;
+            Instantiate(shotPrefab, BarrelEnd.position, BarrelEnd.rotation);
+        }
+            
+    }
+}
---
+++ Assets/Argentics/Scripts/LinearProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Argentics;
+using UnityEngine;
+
+public class LinearProjectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward*speed*Time.deltaTime, Space.Self);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<IDamageable>().Damage();
+            Destroy(gameObject);
+        }
+    }
+}
