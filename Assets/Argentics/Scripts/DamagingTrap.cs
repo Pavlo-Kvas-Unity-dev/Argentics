d94fee74258d86c8b2fb3c2c9213ac3e4063113a
Pavlo Kvas <kvaspasha@gmail.com>
2020-01-23 11:20:48 +0200

    add DamagingTrap script closes #8

---
+++ Assets/Argentics/Scripts/DamagingTrap.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Collections;
+using UnityEngine;
+
+namespace Argentics
+{
+    public class DamagingTrap : MonoBehaviour
+    {
+        [SerializeField] private float timeBetweenDamages = 1f;
+
+        private float prevHurtTime;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag != "Player") return;
+
+            ApplyDamage(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.tag != "Player") return;
+
+            //check if player should be hurt again
+            if (Time.time - prevHurtTime < timeBetweenDamages) return;
+
+            ApplyDamage(other);
+        }
+
+        private void ApplyDamage(Collider other)
+        {
+            var damageable = other.GetComponent<IDamageable>();
+            damageable.Damage();
+
+            //record the damage time
+            prevHurtTime = Time.time;
+        }
+    }
+}
