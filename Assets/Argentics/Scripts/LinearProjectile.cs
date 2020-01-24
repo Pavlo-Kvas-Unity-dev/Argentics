e444b568e46a1518e9d5692acf393c478a4edaba
Pavlo Kvas <kvaspasha@gmail.com>
2020-01-24 12:56:10 +0200

    autodestroy bullets

--- Assets/Argentics/Scripts/LinearProjectile.cs
+++ Assets/Argentics/Scripts/LinearProjectile.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using Argentics;
 using UnityEngine;
-
+    
 [RequireComponent(typeof(Rigidbody))]
 public class LinearProjectile : MonoBehaviour
 {
+    [SerializeField] private float destroyDistance = 20f;
     [SerializeField] private float speed = 1f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward*speed*Time.deltaTime, Space.Self);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+
+        if (Vector3.Distance(startPos, transform.position) > destroyDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
